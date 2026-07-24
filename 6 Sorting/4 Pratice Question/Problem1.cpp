1dde16a64955dc85e91c737b849b6fe3d3fe127f
Himanshu18078 <himanshubisht18078@gmail.com>
2026-07-24 10:36:26 +0530

    Problem on Sorting

--- 6 Sorting/4 Pratice Question/Problem1.cpp
+++ 6 Sorting/4 Pratice Question/Problem1.cpp
@@ -1,10 +1,30 @@
 // Given an array with N distinct elements, convert the given array to a from where all element are in the range from 0 to N-1. The order of element is the same i.e., 0 is placed in the place of the second smallesst element , 1 is placed for the second smallest element ,..N-1 is placed for the largest element
 #include<iostream>
 #include<vector>
+#include<climits>
 #include<algorithm>
 using namespace std;
 void solution(vector<int> &arr){
-  
+  vector<int> v(arr.size(),0);
+  int minIndex = -1;
+  int x = 0;
+
+  for(int i = 0 ; i < v.size();i++){
+    int min = INT_MAX;
+
+    for(int j = 0 ; j < v.size();j++){
+      if(v[j] == 1) continue;
+
+      if(min > arr[j]){
+        min = arr[j];
+        minIndex = j;
+      }
+    }
+
+    arr[minIndex] = x;
+    x++;
+    v[minIndex] = 1;
+  }
 }
 vector<int> inputVector(){
   cout<<"Pleae enter the size of the vector : ";
