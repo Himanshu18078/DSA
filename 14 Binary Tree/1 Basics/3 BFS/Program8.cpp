d340a91e582f2d94508c8a42e4485ca48f48df51
Himanshu18078 <himanshubisht18078@gmail.com>
2026-08-10 18:45:06 +0530

    Binary Tree Level Order Traversaal

---
+++ 14 Binary Tree/1 Basics/3 BFS/Program8.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<climits>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class TreeNode{
+  public:
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int val){
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+//Function to create a tree
+TreeNode *constructTree(int arr[], int size)
+{
+  if (size == 0) return NULL;
+  queue<TreeNode *> q;
+  TreeNode *root = new TreeNode(arr[0]);
+  q.push(root);
+  int i = 1;
+  int j = 2;
+  while (!q.empty() && i < size)
+  {
+    TreeNode *temp = q.front();
+    q.pop();
+    TreeNode *l;
+    TreeNode *r;
+    if (arr[i] != INT_MIN)l = new TreeNode(arr[i]);
+    else l = NULL;
+    if (j != size && arr[j] != INT_MIN) r = new TreeNode(arr[j]);
+    else r = NULL;
+
+    temp->left = l;
+    temp->right = r;
+    
+    if (l != NULL)
+      q.push(l);
+    if (r != NULL)
+      q.push(r);
+
+    i += 2;
+    j += 2;
+  }
+  return root;
+};
+//Function for Post In-Order Traveral (Iterative)
+void inOrderTraversal(TreeNode* root){
+  stack<TreeNode*> s;
+  TreeNode* node = root;
+  while(!s.empty() || node != NULL){
+    if(node != NULL){
+      s.push(node);
+      node=node->left;
+    }else{
+      TreeNode* temp = s.top();
+      s.pop();
+      cout<<temp->val<<" ";
+      node=temp->right;
+    }
+  }
+}
+int main(){
+  int arr[] = {1,2,3,4,5,6,7};
+  TreeNode* root = constructTree(arr , 7);
+  inOrderTraversal(root);
+}
